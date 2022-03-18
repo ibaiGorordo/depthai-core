6906c080ccffaabce5b32c77e5a92fab274cf891
OanaMariaVatavu <oana@luxonis.com>
2022-03-18 13:27:30 +0200

    Update FW - bug workaround

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "6852cf545fd02003cf106f58fe3a1ad464f5ad34")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "9d0bdee4a9020a8e6f0561a54b04f0eaf5bfa770")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
